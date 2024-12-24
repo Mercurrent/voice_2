7134fb10367d3b3fde26f16d3746d23f73f5d355
Tatiana <tania7cramer@gmail.com>
2024-12-24 00:00:54 +0000

    Add instructions for producing main.exe

--- README.md
+++ README.md
@@ -7,6 +7,21 @@
 
 Интерфейс код, - synthesizer_interface/synthesizer_interface.py
 
+Получить exe для main.py:
+
+```
+pip install -r requirements.txt
+pyinstaller --onefile --windowed --clean --hidden-import=omegaconf main.py
+```
+
+Получить exe для synthesizer_interface.py:
+
+```
+cd synthesizer_interface
+pip install -r requirements.txt
+pyinstaller --onefile --windowed --clean synthesizer_interface.py
+```
+
 
 ## 1) Синтезатор речь работает без интернета. 
 ## 2) Без электронного голоса. 
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import torch
+import os
 import sounddevice as sou_voi
+import sys
 import time
+import torch
 
 language = 'ru'
 id_mode = 'ru_v3'
-sample_rate = 48000 # 48000
-speaker = 'baya' # aidar, baya, kseniya, xenia, random
+sample_rate = 48000  # 48000
+speaker = 'baya'  # aidar, baya, kseniya, xenia, random
 put_accent = True
 put_yo = True
-device = torch.device('cpu') # cpu или gpu
+device = torch.device('cpu')  # cpu или gpu
 text = "Хауди Хо, друзья!!!"
 
 
 def bib_model():
-    model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_tts', language=language, speaker=id_mode)
+    model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_tts', language=language, speaker=id_mode, verbose=False)
     return model
 
 
 def ttss_audio():
-    print(bib_model())
     audio = bib_model().apply_tts(
         text=text,
         speaker=speaker,
@@ -30,7 +31,15 @@ def ttss_audio():
 
 
 if __name__ == '__main__':
-    print(text)
+
+    # torch.hub logs to sys.stderr while windowed app has sys.stderr = None
+    # https://pyinstaller.org/en/stable/common-issues-and-pitfalls.html#sys-stdin-sys-stdout-and-sys-stderr-in-noconsole-windowed-applications-windows-only
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, "w")
+        #sys.stdout = io.TextIOWrapper(sys.stdout.buffer. encoding='utf-8')
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, "w")
+
     bib_model()
     sou_voi.play(ttss_audio(), sample_rate)
     time.sleep(len(ttss_audio()) / sample_rate + 0.2)
